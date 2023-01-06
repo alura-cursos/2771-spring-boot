b2e494f78a23f07b2d32058a02557bf62e05f144
Rodrigo Caneppele <r.caneppele@gmail.com>
2023-01-06 14:57:56 -0300

    Validacoes de integridade

---
+++ src/main/java/med/voll/api/domain/ValidacaoException.java
@@ -0,0 +1,7 @@
+package med.voll.api.domain;
+
+public class ValidacaoException extends RuntimeException {
+    public ValidacaoException(String mensagem) {
+        super(mensagem);
+    }
+}
--- src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
+++ src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
@@ -1,5 +1,7 @@
 package med.voll.api.domain.consulta;
 
+import med.voll.api.domain.ValidacaoException;
+import med.voll.api.domain.medico.Medico;
 import med.voll.api.domain.medico.MedicoRepository;
 import med.voll.api.domain.paciente.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,10 +20,22 @@ public class AgendaDeConsultas {
     private PacienteRepository pacienteRepository;
 
     public void agendar(DadosAgendamentoConsulta dados) {
+        if (!pacienteRepository.existsById(dados.idPaciente())) {
+            throw new ValidacaoException("Id do paciente informado não existe!");
+        }
+
+        if (dados.idMedico() != null && !medicoRepository.existsById(dados.idMedico())) {
+            throw new ValidacaoException("Id do médico informado não existe!");
+        }
+
         var paciente = pacienteRepository.findById(dados.idPaciente()).get();
-        var medico = medicoRepository.findById(dados.idMedico()).get();
+        var medico = escolherMedico(dados);
         var consulta = new Consulta(null, medico, paciente, dados.data());
         consultaRepository.save(consulta);
+    }
+
+    private Medico escolherMedico(DadosAgendamentoConsulta dados) {
+
     }
 
 }
