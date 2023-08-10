0a7b235d4b20122890531dc213285c7462ee43e8
Jose Carlos S. Luz <duca.luz@gmail.com>
2023-08-10 16:19:21 -0300

    Adicionado listagens de cunsultas - cunsultas a realizar - cunsultas canceladas - consultas realizadas

--- src/main/java/med/voll/api/controller/ConsultaController.java
+++ src/main/java/med/voll/api/controller/ConsultaController.java
@@ -3,12 +3,20 @@ package med.voll.api.controller;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import med.voll.api.domain.consulta.AgendaDeConsultas;
+import med.voll.api.domain.consulta.ConsultaRepository;
 import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+import med.voll.api.domain.consulta.DadosDetalhamentoConsulta;
 import med.voll.api.domain.consulta.DadosCancelamentoConsulta;
+
+import java.time.LocalDateTime;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 
 @RestController
 @RequestMapping("consultas")
@@ -18,6 +26,9 @@ public class ConsultaController {
     @Autowired
     private AgendaDeConsultas agenda;
 
+    @Autowired
+    private ConsultaRepository repository;
+
     @PostMapping
     @Transactional
     public ResponseEntity agendar(@RequestBody @Valid DadosAgendamentoConsulta dados) {
@@ -25,11 +36,40 @@ public class ConsultaController {
         return ResponseEntity.ok(dto);
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity detalhar(@PathVariable Long id) {
+        var consulta = repository.getReferenceById(id);
+        return ResponseEntity.ok(new DadosDetalhamentoConsulta(consulta));
+    }
+
     @DeleteMapping
     @Transactional
     public ResponseEntity cancelar(@RequestBody @Valid DadosCancelamentoConsulta dados) {
         agenda.cancelar(dados);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<DadosDetalhamentoConsulta>> listar(@PageableDefault(size = 10, sort = {"id"}) Pageable paginacao) {
+
+        var agora = LocalDateTime.now();
+        var page = repository.findAllByMotivoCancelamentoIsNullAndDataGreaterThanEqual(agora, paginacao).map(DadosDetalhamentoConsulta::new);
+        return ResponseEntity.ok(page);
+    }
+
+    @GetMapping("/canceladas")
+    public ResponseEntity<Page<DadosDetalhamentoConsulta>> listarCanceladas(@PageableDefault(size = 10, sort = {"id"}) Pageable paginacao) {
+
+        var page = repository.findAllByMotivoCancelamentoIsNotNull(paginacao).map(DadosDetalhamentoConsulta::new);
+        return ResponseEntity.ok(page);
+    }
+
+    @GetMapping("/realizadas")
+    public ResponseEntity<Page<DadosDetalhamentoConsulta>> listarRealizadas(@PageableDefault(size = 10, sort = {"id"}) Pageable paginacao) {
+
+        var agora = LocalDateTime.now();
+        var page = repository.findAllByMotivoCancelamentoIsNullAndDataLessThanEqual(agora, paginacao).map(DadosDetalhamentoConsulta::new);
+        return ResponseEntity.ok(page);
     }
 
 }
--- src/main/java/med/voll/api/domain/consulta/ConsultaRepository.java
+++ src/main/java/med/voll/api/domain/consulta/ConsultaRepository.java
@@ -1,10 +1,17 @@
 package med.voll.api.domain.consulta;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDateTime;
 
 public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
+    Page<Consulta> findAllByMotivoCancelamentoIsNullAndDataGreaterThanEqual(LocalDateTime data, Pageable paginacao);
+    
+    Page<Consulta> findAllByMotivoCancelamentoIsNullAndDataLessThanEqual(LocalDateTime data, Pageable paginacao);
+    
+    Page<Consulta> findAllByMotivoCancelamentoIsNotNull(Pageable paginacao);
 
     boolean existsByPacienteIdAndDataBetween(Long idPaciente, LocalDateTime primeiroHorario, LocalDateTime ultimoHorario);
 
